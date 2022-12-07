ab0ecd75ac9370c4ff0cecfa030d7df743e40a06
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-07 18:32:49 +0300

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        
+        number = 4567;
+        int sum = 0;
+        while(number != 0){
+
+            sum += (number % 10);
+            number/=10;
+        }
+        System.out.println(sum);
     }
 }
