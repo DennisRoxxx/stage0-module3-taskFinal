b446f59d350486d1530b6d695152558dd577017a
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-07 20:38:27 +0300

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        
+        int temperatureFahrenheit = 1;
+        temperatureCelsius = ((temperatureFahrenheit * 9 / 5) + 32);
+
+        BigDecimal x = new BigDecimal(temperatureCelsius);
+        x = BigDecimal.valueOf((((double)temperatureFahrenheit * 9 / 5) + 32));
+
+        System.out.printf("%.1f %n", x);
     }
 }
