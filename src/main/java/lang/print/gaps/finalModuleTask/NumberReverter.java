b37bfde21e8ca6cdfc47c9b85e72fdfeb1d3c25d
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-08 10:31:17 +0300

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
-        number = 337;
+        
         int reversedNumber = 0;
         if (number > 99) {
             while (number != 0) {
